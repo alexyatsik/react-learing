c157065446ba28a756fc833ccf8d9f942a11884c
Alexeii <alexeii.yatsenko@gmail.com>
2022-08-14 21:34:30 +0300

    add key prop to expense item

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,6 +16,7 @@ function Expenses(props) {
         {
           props.data.map(expense =>
             <ExpenseItem
+              key={expense.id}
               title={expense.title}
               amount={expense.amount}
               date={expense.date}
