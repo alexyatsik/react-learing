2ef20f1fe76cf04c05feecbc05bc36eb293c99a7
Alexeii <alexeii.yatsenko@gmail.com>
2022-08-15 02:25:13 +0300

    add conditional content to the expense item in expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,31 +6,30 @@ import {useState} from "react";
 
 function Expenses(props) {
   const [selectedYear, setSelectedYear] = useState('2022');
-  const [filteredData, setFilteredData] = useState(props.data.filter(item => {
-    return parseInt(item.date.getFullYear()) === parseInt(selectedYear);
-  }));
+
   const getSelectedYearHandler = (year) => {
     setSelectedYear(year);
-    filterData(year);
   }
-  const filterData = (year) => {
-    setFilteredData(props.data.filter(item => {
-      return parseInt(item.date.getFullYear()) === parseInt(year);
-    }));
-  }
+
+  const filteredData = props.data.filter(item => {
+    return item.date.getFullYear().toString() === selectedYear;
+  });
+
+  let expensesContent = filteredData.map(expense =>
+    <ExpenseItem
+      key={expense.id}
+      title={expense.title}
+      amount={expense.amount}
+      date={expense.date}
+    />);
+  if (filteredData.length === 0)
+    expensesContent = <p>No expenses found.</p>
+
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter selectedYear={selectedYear} getSelectedYear={getSelectedYearHandler}/>
-        {
-          filteredData.map(expense =>
-            <ExpenseItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />)
-        }
+        {expensesContent}
       </Card>
     </div>
   );
