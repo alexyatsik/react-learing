63ce0d49202d7a97b4782044f0d40072799fff01
Alexeii <alexeii.yatsenko@gmail.com>
2022-08-16 22:59:40 +0300

    save state

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,5 +1,19 @@
-const ChartBar = () => {
-  return <div>ChartBar</div>;
+const ChartBar = (props) => {
+  let innerFillHeight = '0%';
+
+  if(props.maxValue > 0)
+    innerFillHeight = Math.round(props.value / props.maxValue * 100) + '%';
+
+  return (
+    <div>
+      <div className="chart-bar">
+        <div className="chart-bar__inner">
+          <div className="chart-bar__fill" style={{height: innerFillHeight}} />
+        </div>
+        <div className="chart-bar__label">{props.label}</div>
+      </div>
+    </div>
+  );
 }
 
 export { ChartBar };
